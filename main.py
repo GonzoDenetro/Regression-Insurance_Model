2e1b3223cb8deed37caaf500639dfaa06516b6c0
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2024-10-15 10:47:38 -0600

    Train model

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 def run():
     #LOAD DATA
@@ -18,11 +19,11 @@ def run():
     df = df[(df['bmi'] < max_bmi) & (df['bmi'] > min_bmi)]
     
     #SPLIT FEATURES
-    X = df[['age', 'bmi', 'children', 'sex_male', 'smoker_yes']].values.T
-    Y = df['charges'].values.T
+    X = df[['age', 'bmi', 'children', 'sex_male', 'smoker_yes']].values
+    Y = df['charges'].values.reshape(-1, 1)
     
     #SPLIT TRAIN & TEST SET    
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=104, test_size=0.25)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=42, test_size=0.25)
     
     #SCALE DATA
     x_scaler = StandardScaler().fit(X)
@@ -32,6 +33,13 @@ def run():
     X_test = x_scaler.transform(X_test)
     Y_train = y_scaler.transform(Y_train)
     Y_test = y_scaler.transform(Y_test)
+    
+    #MODEL
+    model = LinearRegression()
+    model.fit(X_train, Y_train) #Train model
+    
+    y_pred = model.predict(X_test)
+    print(y_pred[:20])
 
 if __name__ == '__main__':
     run()
