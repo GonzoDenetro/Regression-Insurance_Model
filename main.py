18bce3aedc207fd6757d353c09a8fda2168046b9
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2024-10-15 10:01:06 -0600

    Standarize data

--- main.py
+++ main.py
@@ -23,6 +23,15 @@ def run():
     
     #SPLIT TRAIN & TEST SET    
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=104, test_size=0.25)
+    
+    #SCALE DATA
+    x_scaler = StandardScaler().fit(X)
+    y_scaler = StandardScaler().fit(Y)
+    
+    X_train = x_scaler.transform(X_train)
+    X_test = x_scaler.transform(X_test)
+    Y_train = y_scaler.transform(Y_train)
+    Y_test = y_scaler.transform(Y_test)
 
 if __name__ == '__main__':
     run()
